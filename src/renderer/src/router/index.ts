9cc1fd6cf6dffd861cf105b8437f5bff76e4fe2f
Git User <707038137@qq.com>
2022-10-16 02:06:57 +0800

    pref: add router & store

---
+++ src/renderer/src/router/index.ts
@@ -0,0 +1,13 @@
+import {Home, About} from "../views";
+
+import {createRouter, createWebHashHistory} from "vue-router";
+
+export const routes = [
+  {path: "/", component: Home, name: "home"},
+  {path: "/about", component: About, name: "about"},
+];
+
+export const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
